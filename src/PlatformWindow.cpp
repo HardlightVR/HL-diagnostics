1ebfa0ff18a416cbdda233419a5dd30398e40eff
Casey Waldren <caseywaldren@gmail.com>
2017-10-05 18:11:50 -0700

    Add emulation

--- src/PlatformWindow.cpp
+++ src/PlatformWindow.cpp
@@ -3,6 +3,7 @@
 #include "imgui.h"
 #include <vector>
 #include <sstream>
+#include <unordered_map>
 //
 //hvr_diagnostics_ui menu;
 //menu.keyval = [](const char* key, const char* val) {
@@ -41,6 +42,39 @@ static const char* const node_concepts[] = {
 	"AbsoluteTracker"
 };
 
+static const std::unordered_map<uint32_t, const char*> region_names = {
+	{ 0,  "unknown" },
+	{ 1 * NSVR_SUBREGION_BLOCK , "body" },
+	{ 2 * NSVR_SUBREGION_BLOCK , "torso" },
+	{ 3 * NSVR_SUBREGION_BLOCK , "torso_front" },
+	{ 4 * NSVR_SUBREGION_BLOCK , "chest_left" },
+	{ 5 * NSVR_SUBREGION_BLOCK , "chest_right" },
+	{ 6 * NSVR_SUBREGION_BLOCK , "upper_ab_left" },
+	{ 7 * NSVR_SUBREGION_BLOCK , "middle_ab_left" },
+	{ 8 * NSVR_SUBREGION_BLOCK , "lower_ab_left" },
+	{ 9 * NSVR_SUBREGION_BLOCK , "upper_ab_right" },
+	{ 10 * NSVR_SUBREGION_BLOCK , "middle_ab_right" },
+	{ 11 * NSVR_SUBREGION_BLOCK , "lower_ab_right" },
+	{ 12 * NSVR_SUBREGION_BLOCK , "torso_back" },
+	{ 13 * NSVR_SUBREGION_BLOCK , "torso_left" },
+	{ 14 * NSVR_SUBREGION_BLOCK , "torso_right" },
+	{ 15 * NSVR_SUBREGION_BLOCK , "upper_back_left" },
+	{ 16 * NSVR_SUBREGION_BLOCK , "upper_back_right" },
+	{ 17 * NSVR_SUBREGION_BLOCK , "upper_arm_left" },
+	{ 18 * NSVR_SUBREGION_BLOCK , "lower_arm_left" },
+	{ 19 * NSVR_SUBREGION_BLOCK , "upper_arm_right" },
+	{ 20 * NSVR_SUBREGION_BLOCK , "lower_arm_right" },
+	{ 21 * NSVR_SUBREGION_BLOCK , "shoulder_left" },
+	{ 22 * NSVR_SUBREGION_BLOCK , "shoulder_right" },
+	{ 23 * NSVR_SUBREGION_BLOCK , "upper_leg_left" },
+	{ 24 * NSVR_SUBREGION_BLOCK , "lower_leg_left" },
+	{ 25 * NSVR_SUBREGION_BLOCK , "upper_leg_right" },
+	{ 26 * NSVR_SUBREGION_BLOCK , "lower_leg_right" },
+	{ 27 * NSVR_SUBREGION_BLOCK , "head" },
+	{ 28 * NSVR_SUBREGION_BLOCK , "palm_left" },
+	{ 29 * NSVR_SUBREGION_BLOCK , "palm_right" },
+};
+
 PlatformWindow::PlatformWindow(hvr_platform * platform, NSVR_System* plugin)
 	: m_platform(platform)
 	, m_plugin(plugin)
@@ -54,13 +88,22 @@ PlatformWindow::PlatformWindow(hvr_platform * platform, NSVR_System* plugin)
 	};
 
 	hvr_platform_setupdiagnostics(m_platform, &menu);
+
+	NSVR_BodyView_Create(&m_retainedBodyview);
 }
 
 void PlatformWindow::Render()
 {
+
+	renderEmulation();
 	renderPlatformSide();
 	renderPluginSide();
 
+}
+
+PlatformWindow::~PlatformWindow()
+{
+	NSVR_BodyView_Release(&m_retainedBodyview);
 }
 
 void renderPlatformVersion()
@@ -136,6 +179,58 @@ void PlatformWindow::renderPluginSide()
 		ImGui::EndChild();
 		ImGui::PopStyleVar();
 	}
+}
+
+void PlatformWindow::renderEmulation()
+{
+
+	NSVR_BodyView_Poll(m_retainedBodyview, m_plugin);
+
+	uint32_t numNodes = 0;
+	NSVR_BodyView_GetNodeCount(m_retainedBodyview, &numNodes);
+
+	ImGui::Begin("Body view");
+
+	ImGui::Text("Displays activity on regions registered by the hardware devices."); ImGui::SameLine();
+	ShowHelpMarker("The view is populated lazily when you begin to interact or send commands to the platform. Don't worry if it's blank");
+
+	for (auto i = 0; i < numNodes; i++) {
+	
+
+		uint32_t region = nsvr_region_unknown;
+		NSVR_BodyView_GetNodeRegion(m_retainedBodyview, i, &region);
+		ImGui::PushStyleColor(ImGuiCol_FrameBg, (ImVec4)ImColor::HSV(i / 7.0f, 0.5f, 0.5f));
+		ImGui::PushStyleColor(ImGuiCol_FrameBgHovered, (ImVec4)ImColor::HSV(i / 7.0f, 0.6f, 0.5f));
+		ImGui::PushStyleColor(ImGuiCol_FrameBgActive, (ImVec4)ImColor::HSV(i / 7.0f, 0.7f, 0.5f));
+		ImGui::PushStyleColor(ImGuiCol_SliderGrab, (ImVec4)ImColor::HSV(i / 7.0f, 0.9f, 0.9f));
+		float intensity = 0;
+		NSVR_BodyView_GetIntensity(m_retainedBodyview, i, &intensity);
+		ImGui::VSliderFloat(std::string("##"+std::string(region_names.at(region))).c_str(), ImVec2(25, 40), &intensity, 0, 1.0, "%.1f");
+
+		//ImGui::VSliderFloat(region_names.at(region), ImVec2(25, 40), &intensity, 0, 1.0, "%.1f");
+		ImGui::PopStyleColor(4);
+
+		if ((i % 4) < 3) ImGui::SameLine();
+	}
+	ImGui::End();
+
+
+
+	/*static bool selected[16] = { true, false, false, false, false, true, false, false, false, false, true, false, false, false, false, true };
+	for (int i = 0; i < 16; i++)
+	{
+		ImGui::PushID(i);
+		if (ImGui::Selectable("Sailor", &selected[i], 0, ImVec2(50, 50)))
+		{
+			int x = i % 4, y = i / 4;
+			if (x > 0) selected[i - 1] ^= 1;
+			if (x < 3) selected[i + 1] ^= 1;
+			if (y > 0) selected[i - 4] ^= 1;
+			if (y < 3) selected[i + 4] ^= 1;
+		}
+		if ((i % 4) < 3) ImGui::SameLine();
+		ImGui::PopID();
+	}*/
 }
 
 void PlatformWindow::renderPlatformUI()
--- src/PlatformWindow.h
+++ src/PlatformWindow.h
@@ -2,6 +2,7 @@
 
 #include "NSDriverApi.h"
 #include "NSLoader.h"
+#include "NSLoader_Internal.h"
 static void ShowHelpMarker(const char* desc);
 
 
@@ -9,16 +10,20 @@ class PlatformWindow {
 public:
 	PlatformWindow(hvr_platform* platform, NSVR_System* plugin);
 	void Render();
-
+	~PlatformWindow();
 	
 
 private:
 	hvr_platform* m_platform;
 	NSVR_System* m_plugin;
+
+	NSVR_BodyView* m_retainedBodyview;
 	void renderPlatformUI();
 	void renderAvailableHardwarePlugins();
 
 	void renderPlatformSide();
 	void renderPluginSide();
+
+	void renderEmulation();
 
 };
